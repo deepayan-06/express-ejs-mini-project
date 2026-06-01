a39f1368c7c6fa6c572c797c7f0f9faa774e45e5
Deepayan Banerjee <deepayanbanerjee74@gmail.com>
2026-06-01 09:22:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname,"/public/css")));
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"/views"));
 
-
+// Home-Route //
 app.get("/home", (req, res) => {
     res.render("home");
 })
